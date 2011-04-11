6bb585669bfeaa153ae09efcc105c9754a7438d2
David Morris <davidrowlandmorris@gmail.com>
2011-04-11 10:06:45 +0100

    Add perl stuff to ccl-init.el

--- ccl/ccl-init.el
+++ ccl/ccl-init.el
@@ -38,8 +38,28 @@
   "/*-------------------------------------------"
   "----------------------------------" \n
   "" _ \n
-  "-----------------------------------------------------------------------------*/" \n
-  \n \n)
+  "---------------------------------------------"
+  "--------------------------------*/" \n)
+
+(define-skeleton ccl-perl-file-header
+  "Insert a CCL Perl File Header" nil
+  "#############################################"
+  "##################################" \n
+  "# (C) Copyright Cambridge Consultants, " (format-time-string "%Y") \n
+  "# " \n
+  "# Cambridge Consultants Project Reference " _ \n
+  "# " \n
+  "# FILE" \n
+  "#    " (buffer-name) \n
+  "# " \n
+  "# ORIGINAL AUTHOR" \n
+  "#    David Morris" \n
+  "# " \n
+  "# DESCRIPTION" \n
+  "# " \n
+  "# " \n
+  "#############################################"
+  "##################################" \n \n)
 
 (defun drm-insert-skeleton-with-indent (skeleton)
   "Insert skeleton, then, once the whole thing has been inserted, reindent it.
@@ -56,8 +76,9 @@ unless horrible manual things are done."
   "Insert one of various styles of CCL comment header"
   (interactive "SComment Header Style: ")
   (cond 
-   ((eq style 'file) (drm-insert-skeleton-with-indent 'ccl-c-file-header))
-   ((eq style 'section) (drm-insert-skeleton-with-indent 'ccl-c-section-header))
+   ((eq style 'cfile) (drm-insert-skeleton-with-indent 'ccl-c-file-header))
+   ((eq style 'csection) (drm-insert-skeleton-with-indent 'ccl-c-section-header))
+   ((eq style 'perlfile) (drm-insert-skeleton-with-indent 'ccl-perl-file-header))
    (t (message "Unknown header style"))))
   
 
@@ -102,7 +123,7 @@ unless horrible manual things are done."
 ;; epic boilerplate
 (defun ccl-insert-c-header-boilerplate ()
   (interactive)
-  (ccl-comment-header 'file)
+  (ccl-comment-header 'cfile)
   (goto-char (point-max))
   (drm-insert-include-guard)
   (mapc
@@ -118,7 +139,7 @@ unless horrible manual things are done."
 
 (defun ccl-insert-c-implementation-boilerplate ()
   (interactive)
-  (ccl-comment-header 'file)
+  (ccl-comment-header 'cfile)
   (goto-char (point-max))
   (mapc
    'ccl-insert-c-section-header
@@ -133,3 +154,52 @@ unless horrible manual things are done."
      "Function implementations"
      "End of file")))
 
+
+(defun ccl-insert-perl-boilerplate ()
+  (interactive)
+  (insert "#! perl\n")
+  (ccl-comment-header 'perlfile)
+  (goto-char (point-max))
+  (insert
+   (concat
+    "\n"
+    "use strict;\n"
+    "use warnings;\n"
+    "\n")))
+  
+
+(defun ccl-insert-pod-boilerplate ()
+  (interactive)
+  (goto-char (point-max))
+  (insert
+   (concat
+    "=head1 NAME\n"
+    "\n"
+    "=head1 SYNOPSIS\n"
+    "\n"
+    "=head1 DESCRIPTION\n"
+    "\n"
+    "=head1 OPTIONS\n"
+    "\n"
+    "=item B<--setting>=I<value>\n"
+    "\n"
+    "=head1 EXAMPLES\n"
+    "\n"
+    "=head1 CAVEATS\n"
+    "\n"
+    "=head1 BUGS\n"
+    "\n"
+    "=head1 RESTRICTIONS\n"
+    "\n"
+    "=head1 NOTES\n"
+    "\n")))
+
+
+;; boilerplate keybindings
+(define-prefix-command 'ccl-boilerplate-map)
+(global-set-key (kbd "C-c s b") 'ccl-boilerplate-map)
+(global-set-key (kbd "C-c s b h") 'ccl-insert-c-header-boilerplate)
+(global-set-key (kbd "C-c s b i") 'ccl-insert-c-implementation-boilerplate)
+(global-set-key (kbd "C-c s b p") 'ccl-insert-perl-boilerplate)
+(global-set-key (kbd "C-c s b f") (lambda () (interactive)
+                                    (ccl-insert-c-function-header "" "" "")))
