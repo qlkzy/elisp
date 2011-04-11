56ea08deb5aea3c46ea7f52aa154309c3972f66d
David Morris <davidrowlandmorris@gmail.com>
2011-04-11 09:59:06 +0100

    Add rename-file-and-buffer

--- misc/misc-init.el
+++ misc/misc-init.el
@@ -352,6 +352,23 @@ enough room"
   (insert "on")
   (upcase-region (point) (+ (point) 1)))
 
-(load-file "~/elisp/misc/misc-keys.el")
+;;
+;; Never understood why Emacs doesn't have this function.
+;;
+(defun rename-file-and-buffer (new-name)
+  "Renames both current buffer and file it's visiting to NEW-NAME."
+  (interactive "sNew name: ")
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (if (not filename)
+        (message "Buffer '%s' is not visiting a file!" name)
+      (if (get-buffer new-name)
+          (message "A buffer named '%s' already exists!" new-name)
+        (progn 
+          (rename-file name new-name 1) 
+          (rename-buffer new-name)
+          (set-visited-file-name new-name)
+          (set-buffer-modified-p nil))))))
 
+(load-file "~/elisp/misc/misc-keys.el")
 
