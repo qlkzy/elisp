baf4859597f0f77940f7c0eec1670ea26bfef54f
David Morris <davidrowlandmorris@gmail.com>
2011-04-12 13:25:11 +0100

    Add info stuff, incl perl

---
+++ info/info-init.el
@@ -0,0 +1,5 @@
+(add-to-list 'Info-directory-list (concat invocation-directory "../info"))
+(setq Info-additional-directory-list Info-directory-list)
+(add-to-list 'Info-additional-directory-list (drm-custom-path "info/perl/"))
+(setq Info-directory-list (list (drm-custom-path "info/")))
+
