38f40c5a1e290acf6a2c2fca19155210f45e9767
David Morris <drm511@cs.york.ac.uk>
2011-11-29 17:02:29 +0000

    Remove w3-mode

--- w3-mode/w3-mode-init.el
+++
@@ -1,8 +0,0 @@
-;;; Emacs/W3 Configuration
-
-(progn
-  (cd "~/elisp/w3-mode/w3")
-  (normal-top-level-add-subdirs-to-load-path)
-  (cd "~/"))
-
-(condition-case () (require 'w3-auto "w3-auto") (error nil))
