79f3b06ff96d8b9ec780d1c09c175f797ddda980
David Morris <drm511@cs.york.ac.uk>
2011-11-29 16:48:26 +0000

    Remove info files

--- info/info-init.el
+++
@@ -1,6 +0,0 @@
-(add-to-list 'Info-directory-list (concat invocation-directory "../info"))
-(setq Info-additional-directory-list Info-directory-list)
-(add-to-list 'Info-additional-directory-list (drm-custom-path "info/perl/"))
-(add-to-list 'Info-additional-directory-list (drm-custom-path "info/bash/"))
-(setq Info-directory-list (list (drm-custom-path "info/")))
-
