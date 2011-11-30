f3e1a71d3bf02748279f9b37b1892e3d3ab85eec
David Morris <davidrowlandmorris@gmail.com>
2011-11-30 11:08:11 +0000

    Couple of skeletons for rhtml

--- rails/rails-init.el
+++ rails/rails-init.el
@@ -1,7 +1,20 @@
 (add-to-list 'load-path (drm-custom-path "rails/rinari"))
 (require 'rinari)
 
+(define-skeleton rhtml-skeleton-div
+  "" nil
+  > "<div class=\""_"\">"\n
+  > _ \n
+  > -2 "</div>"\n)
+
+(defun drm-rhtml-init ()
+  (rinari-launch)
+  (abbrev-def-list local-abbrev-table
+    ("div" "" 'rhtml-skeleton-div)
+    ("br" "<br/>" nil)))
+
 (add-to-list 'load-path (drm-custom-path "rails/rhtml"))
 (require 'rhtml-mode)
-(add-hook 'rhtml-mode-hook
-     	  (lambda () (rinari-launch)))
+(add-hook 'rhtml-mode-hook 'drm-rhtml-init)
+
+
