9a3b2e073ff5a8de53316c6eae779d19cca65cb6
David Morris <davidrowlandmorris@gmail.com>
2012-01-04 00:58:28 +0000

    Add some more RHTML skeletons

--- rails/rails-init.el
+++ rails/rails-init.el
@@ -1,11 +1,21 @@
 (add-to-list 'load-path (drm-custom-path "rails/rinari"))
 (require 'rinari)
 
-(define-skeleton rhtml-skeleton-div
+(define-skeleton html-skeleton-div
   "" nil
   > "<div class=\""_"\">"\n
   > _ \n
   > -2 "</div>"\n)
+
+(define-skeleton html-skeleton-span
+  "" nil
+  > "<span class=\""_"\">"_"</span>"\n)
+
+(define-skeleton html-skeleton-ul
+  "" nil
+  > "<ul>" \n
+  > "<li>"_"</li>" \n
+  > -2 "</ul>" \n)
 
 (define-skeleton rhtml-skeleton-if
   "" nil
@@ -15,13 +25,26 @@
   > "" \n
   > -2 "<% end %>"\n)
 
+(defun html-paragraphify ()
+  (let ((p (point)))
+    (save-excursion
+      (insert "<p>")
+      (forward-paragraph)
+      (insert "</p>")
+      (setq p (point)))
+    (indent-region (point) p))
+  (indent-line))
+
 (defun drm-rhtml-init ()
   (rinari-launch)
   (abbrev-def-list local-abbrev-table
-    ("rbif" "" 'rhtml-skeleton-if)
-    ("div" "" 'rhtml-skeleton-div)
+    ("rbif" "" 'html-skeleton-if)
+    ("div" "" 'html-skeleton-div)
+    ("span" "" 'html-skeleton-span)
     ("br" "<br/>" nil)
-    ("hr" "<hr/>\n" nil)))
+    ("hr" "<hr/>\n" nil)
+    ("ul" "" 'html-skeleton-ul)
+    ("pp" "" 'html-paragraphify)))
 
 (add-to-list 'load-path (drm-custom-path "rails/rhtml"))
 (require 'rhtml-mode)
