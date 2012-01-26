a2280b2157b41a1ceca95e188668c129e68489df
David Morris <drm511@cs.york.ac.uk>
2012-01-26 17:12:20 +0000

    Snippets for vhdl-mode

--- yasnippet/yasnippet-init.el
+++ yasnippet/yasnippet-init.el
@@ -3,3 +3,5 @@
 (require 'yasnippet) ;; not yasnippet-bundle
 (setq yas/prompt-functions '(yas/x-prompt yas/ido-prompt yas/completing-prompt))
 (yas/global-mode 1)
+(add-to-list 'yas/snippet-dirs
+             (drm-custom-path "yasnippet/snippets"))
