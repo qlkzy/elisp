b1e6ad15dd7956e8885cca14f1d7faffd6358ba3
David Morris <davidrowlandmorris@gmail.com>
2012-02-14 00:54:16 +0000

    Add keybindings to make it easier to resize windows

--- misc/misc-keys.el
+++ misc/misc-keys.el
@@ -69,6 +69,12 @@
 (global-set-key (kbd "M-Z") 'zap-to-char)
 
 
+(global-set-key (kbd "S-C-<left>") 'shrink-window-horizontally)
+(global-set-key (kbd "S-C-<right>") 'enlarge-window-horizontally)
+(global-set-key (kbd "S-C-<down>") 'shrink-window)
+(global-set-key (kbd "S-C-<up>") 'enlarge-window)
+
+
 ;; Tags
 
 ;; (define-prefix-command 'drm-tags-map)
