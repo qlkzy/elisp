f42074ad240f7ab2f7b7fae21056ce28f3b8b6de
David Morris <davidrowlandmorris@gmail.com>
2012-07-25 14:54:09 +0100

    Test compilation shortcut

--- quick.el
+++ quick.el
@@ -1,2 +1,9 @@
 (define-prefix-command 'drm-leader-key)
 (global-set-key (kbd "C-,") 'drm-leader-key)
+(global-set-key (kbd "C-, ,") 'drm-compile-tests)
+
+(defun drm-compile-tests ()
+  (interactive)
+  (if (string-match "test/$" default-directory)
+      (compile "make -C .. test")
+    (compile "make test")))
