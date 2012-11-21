b373efed8ada38c75d162c6fee4b9f31da7098a7
David Morris <davidrowlandmorris@gmail.com>
2012-11-21 11:41:09 +0000

    Add CL Hyperspec Linking to SLIME

--- slime/slime-init.el
+++ slime/slime-init.el
@@ -1,3 +1,9 @@
 (setq inferior-lisp-program "sbcl")
 (require 'slime)
 (slime-setup)
+(require 'info-look)
+(info-lookup-add-help
+ :mode 'lisp-mode
+ :regexp "[^][()'\" \t\n]+"
+ :ignore-case t
+ :doc-spec '(("(ansicl)Symbol Index" nil nil nil)))
