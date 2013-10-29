3118126c4e728e620874b51bb24f2827dac37a30
DR Morris <drm511@pc812s.cs.york.ac.uk>
2013-10-29 11:12:44 +0000

    Make colours work on emacs 23

--- tomorrow-theme/tomorrow-theme-init.el
+++ tomorrow-theme/tomorrow-theme-init.el
@@ -1,2 +1,6 @@
-(add-to-list 'custom-theme-load-path (drm-custom-path "tomorrow-theme/"))
-(load-theme 'tomorrow-night t)
+(cond
+ ((>= emacs-major-version 24)
+  (add-to-list 'custom-theme-load-path (drm-custom-path "tomorrow-theme/"))
+  (load-theme 'tomorrow-night t))
+ (t (drm-custom-load "tomorrow-theme/color-theme-tomorrow")
+    (color-theme-tomorrow-night)))
